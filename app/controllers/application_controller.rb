658638745bdd236c2cf49ad36728a986b5336ca0
Tetsushi Sugano <tetsushi.sugano@gmail.com>
2017-07-01 16:17:27 +0900

    sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit(:name, :profile, :email, :password, :password_confirmation, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :email, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :email, :password, :password_confirmation, :current_password])
   end
 end
